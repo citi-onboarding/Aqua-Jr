74f7bac7bd5b3cd9a0058de4d7e747af9687863d
sofiadinizms <sdms@cin.ufpe.br>
2020-07-01 18:07:30 -0300

    added Banner database

---
+++ server/models/Banner.js
@@ -0,0 +1,14 @@
+const keystone = require('keystone');
+
+const { Types } = keystone.Field;
+
+const Banner = new keystone.List('Banners');
+
+Banner.add({
+    image:{ type: Types.CloudinaryImages ,initial: true, required: true },
+    title:{ type: Types.text, initial: true, required: true},
+    text: { type: Types.text, initial: true, required: true}
+});
+
+Banner.register();
+
