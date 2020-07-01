458f89a423267aa80bfd490ee060ad1973593874
sofiadinizms <sdms@cin.ufpe.br>
2020-07-01 19:26:04 -0300

    Feature(feature/Banner_and_Post_To_Blog_): Finalized the Banner and Post to Blog databases

--- server/models/Banner.js
+++ server/models/Banner.js
@@ -3,9 +3,9 @@ const { Types } = keystone.Field;
 const Banner = new keystone.List('Banners');
 
 Banner.add({
-    image:{ type: Types.CloudinaryImages ,initial: true, required: true },
     title:{ type: Types.Text, initial: true, required: true },
-    text: { type: Types.Text, initial: true, required: true }
+    text: { type: Types.Text, initial: true, required: true },
+    image:{ type: Types.CloudinaryImages ,initial: true, required: true }
 });
 
 Banner.register();
