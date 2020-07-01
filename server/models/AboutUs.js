f49df0c8cdb7d47011f862b9c9a36d9ed85ab0c8
Vinícius Scala <viniciusscala@gmail.com>
2020-07-01 19:20:45 -0300

    Feature(feature/aboutUs-testimony-dataBase-routes):correct a typo in AboutUs.js

--- server/models/AboutUs.js
+++ server/models/AboutUs.js
@@ -6,7 +6,7 @@ const About = new keystone.List("AboutUs");
 
 About.add({
     image: {type: Types.CloudinaryImages, initial: true, required: true },
-    text: {type: Types.text, initial: true, required: true}
+    text: {type: Types.Text, initial: true, required: true}
 });
 
 About.register();
---
+++ server/models/Testimony.js
@@ -0,0 +1,12 @@
+// const keystone = require("keystone");
+
+// const {Types} = keystone.Field;
+
+// const Testimony = new keystone.List("Testimonies");
+
+// Testimony.add({
+//     image: {type: Types.CloudinaryImages, initial: true, required: true },
+//     text: {type: Types.text, initial: true, required: true}
+// });
+
+// Testimony.register();
