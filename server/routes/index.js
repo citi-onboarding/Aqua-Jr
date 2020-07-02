a4e4524dc8652c9c9e9f7b6974c180e1eca73c90
Vinícius Scala <viniciusscala@gmail.com>
2020-07-02 15:46:38 -0300

    Feature(aboutUs-testimony-dataBase-routes):add about api route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const keystone = require('keystone');
 const cors = require('cors');
 
 const Example = keystone.list('Examples');
+const About = keystone.List("AboutUs");
 
 module.exports = (app) => {
   app.use(cors());
@@ -15,6 +16,16 @@ module.exports = (app) => {
     Example.model.find((err, items) => {
       if (err) return res.apiError('database error', err);
       res.send(items);
+    });
+  });
+
+  app.get('/api/about', (req, res) => {
+    About.model.find((err, itens) => {
+      if(err){
+        return res.apiError('database error', err);
+      }else{
+        res.send(itens);
+      }
     });
   });
 };
