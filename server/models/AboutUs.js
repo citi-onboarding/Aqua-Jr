a8865bfb133e0574561ff3872609466b5841b866
Vinícius Scala <viniciusscala@gmail.com>
2020-07-02 18:37:29 -0300

    Feature(aboutUs-testimony-dataBase-routes):correct the bug that would allow to upload more than one img at once and the index option

--- server/models/AboutUs.js
+++ server/models/AboutUs.js
@@ -5,8 +5,8 @@ const {Types} = keystone.Field;
 const About = new keystone.List("AboutUs");
 
 About.add({
-    image: {type: Types.CloudinaryImages, initial: true, required: true },
-    text: {type: Types.Text, initial: true, required: true}
+    image: {type: Types.CloudinaryImage, initial: true, required: true },
+    text: {type: Types.Text, initial: true, required: true, index: true}
 });
 
 About.register();
--- server/models/Testimony.js
+++ server/models/Testimony.js
@@ -1,12 +1,12 @@
 const keystone = require("keystone");
 
-const {Types} = keystone.Field;
+const { Types } = keystone.Field;
 
 const Testimony = new keystone.List("Testimonies");
 
 Testimony.add({
-    image: {type: Types.CloudinaryImages, initial: true, required: true },
-    text: {type: Types.Text, initial: true, required: true},
+    image: {type: Types.CloudinaryImage, initial: true, required: true },
+    text: {type: Types.Text, initial: true, required: true, index: true},
     description: {type: Types.Text, initial: true, required: true}
 });
 
