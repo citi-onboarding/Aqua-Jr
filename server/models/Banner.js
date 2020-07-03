e9e2f922d9ae0b49219c84ce95304590d30c390b
sofiadinizms <sdms@cin.ufpe.br>
2020-07-03 20:12:40 -0300

    Feature (feature/Banner_and_Post_To_Blog_) finalised the Banner module

--- server/models/Banner.js
+++ server/models/Banner.js
@@ -5,7 +5,7 @@ const Banner = new keystone.List('Banners');
 Banner.add({
     title:{ type: Types.Text, initial: true, required: true },
     text: { type: Types.Text, initial: true, required: true },
-    image:{ type: Types.CloudinaryImages ,initial: true, required: true }
+    image:{ type: Types.CloudinaryImage ,initial: true, required: true }
 });
 
 Banner.register();
