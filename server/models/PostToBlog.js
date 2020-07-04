d64a88b3be31b8fb983e4ff8ba0d0141f26db303
sofiadinizms <sdms@cin.ufpe.br>
2020-07-04 20:03:29 -0300

    enabled the creation of multiple blog posts

--- server/models/PostToBlog.js
+++ server/models/PostToBlog.js
@@ -3,13 +3,13 @@ const { Types } = keystone.Field;
 
 const PostToBlog = new keystone.List('PostToBlogs',{
   map: { name: 'title' },
-  nocreate: true,
-  nodelete: true
+  nocreate: false,
+  nodelete: false
 });
 
 PostToBlog.add({
-  title: { type: Types.Text, required: true, initial: true, index: true },
-  text: { type: Types.Text, initial: true, required: true }
+  title: { type: Types.Text,initial: true, required: true, index: true },
+  image:{ type: Types.CloudinaryImage ,initial: true, required: true }
 });
 
 PostToBlog.register();
