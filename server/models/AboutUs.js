e2b1462c381f0c8c997fa24f3dfd77d2db67f0d2
Vinícius Scala <viniciusscala@gmail.com>
2020-07-06 21:42:20 -0300

    Hotfix(AboutUs) : Fix the AboutUs db and now it can only have one item and this item has 2 images

--- server/models/AboutUs.js
+++ server/models/AboutUs.js
@@ -5,11 +5,14 @@ const {Types} = keystone.Field;
 const About = new keystone.List("AboutUs",{
     map: { name: 'text'},
     singular: "About",
-    plural: "About Us"
+    plural: "About Us",
+    nocreate: true,
+    nodelete: true
 });
 
 About.add({
-    image: {type: Types.CloudinaryImage, initial: true, required: true},
+    image1: {type: Types.CloudinaryImage, initial: true, required: true},
+    image2: {type: Types.CloudinaryImage, initial: true, required: true},
     text: {type: Types.Text, initial: true, required: true, index: true}
 });
 
