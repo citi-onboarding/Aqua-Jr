0fe4641ff214aa8a8c6397077000d33426ba4fca
sofiadinizms <sdms@cin.ufpe.br>
2020-07-07 15:32:04 -0300

    Hotfix(PostToBlogCategory): Added category to Post To Blog database table

--- server/models/PostToBlog.js
+++ server/models/PostToBlog.js
@@ -9,6 +9,7 @@ const PostToBlog = new keystone.List('PostToBlogs',{
 
 PostToBlog.add({
   title: { type: Types.Text,initial: true, required: true, index: true },
+  category: { type: Types.Text,initial: true, required: true },
   image:{ type: Types.CloudinaryImage ,initial: true, required: true }
 });
 
