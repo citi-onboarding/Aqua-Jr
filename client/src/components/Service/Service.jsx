dd1825d34894c90efa4b99ac17ea95fdc7b4aec3
Vinícius Scala <viniciusscala@gmail.com>
2020-07-08 11:10:04 -0300

    Feature(expertise-area) : Add the code for the Service component

--- client/src/components/Service/Service.jsx
+++ client/src/components/Service/Service.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Service.css';
+import "./Service.css";
 
-function Service(){
-
+function Service(props) {
+  return (
+    <div className="Service">
+      <img src={props.img} alt={"Imagem que representa " + props.title} />
+      <h4>{props.title}</h4>
+      <p>{props.text}</p>
+    </div>
+  );
 }
 
 export default Service;
