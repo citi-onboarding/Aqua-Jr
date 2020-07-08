530b0d93dc00cb55f6dc332a9c44d14d155f5aff
Vinícius Scala <viniciusscala@gmail.com>
2020-07-08 00:33:32 -0300

    Feature(expertise-area) : Add the feature that will display all the categories and filter the services based on them

--- client/src/components/ExpertiseArea/ExpertiseArea.jsx
+++ client/src/components/ExpertiseArea/ExpertiseArea.jsx
@@ -3,15 +3,71 @@ import axios from "axios";
 
 import "./ExpertiseArea.css";
 
+let db = [];
+
 function ExpertiseArea() {
+	const [categories, setCategories] = React.useState([]);
+	const [services, setServices] = React.useState([]);
+  const [selected, setSelected] = React.useState('');
+
+  const loadDb = async () => {
+		// get the data from the db
+		const res = await axios.get("http://localhost:3001/api/areas");
+		db = res.data;
+		// if the db has at least one item...
+    if(db){
+			// set the selected category as the first one
+			setSelected(res.data[0].Category);
+
+			// create an empty array and set it as an array with all the categories names without repeating them
+			const categoriesAux = [];
+			db.forEach((item)=>{
+				if(!categoriesAux.includes(item.Category)){
+					categoriesAux.push(item.Category);
+				}
+			});
+			setCategories(categoriesAux);
+    }
+  };
+
+	//call loadDb when the page is loaded
+  React.useEffect(() => {
+    loadDb();
+	}, []);
+	
+	const handleButtonClick = (e)=>{
+		//set selected as the text within the button clicked
+    setSelected(e.target.innerText);
+  }
+
+	// when selected changes...
+  React.useEffect(() => {
+		//create an aux array and set it as an array with all the items from the t=db that have the selected category
+    const aux = db.filter((item)=>{
+			return item.Category === selected;
+		});
+    setServices(aux);
+  }, [selected]);
+
   return (
     <section className="ExpertiseArea">
       <h2>Áreas de atuação</h2>
-      <div className="categories"></div>
-      <div className="services"></div>
+      <div className="categories">
+        {categories.map((item, index)=>{
+          return <button key={index} onClick={handleButtonClick}>{item}</button>;
+        })}
+      </div>
+      <div className="services">
+        {services.map((item, index)=>{
+					return(
+					<div key={index}>{item.Title}</div>
+					);
+				})}
+      </div>
       <div className="dots"></div>
     </section>
   );
 }
 
 export default ExpertiseArea;
+
