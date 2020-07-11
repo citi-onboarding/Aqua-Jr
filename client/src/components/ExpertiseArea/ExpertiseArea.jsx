7560cc13fdf9f49d8bfeb4df3c166d12356178aa
Vinícius Scala <viniciusscala@gmail.com>
2020-07-11 14:17:18 -0300

    Feature(expertise-area) : Add the styles and finish the web version

--- client/src/components/ExpertiseArea/ExpertiseArea.jsx
+++ client/src/components/ExpertiseArea/ExpertiseArea.jsx
@@ -1,56 +1,86 @@
 import React from "react";
 import axios from "axios";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Service from "../Service";
 
 import "./ExpertiseArea.css";
 
-
+// this let is created outside the function cause I don't want it to be rewritten every time the component is rendered
 let db = [];
 
+// Creating the components for the next and previous arrow
+function NextArrow(props) {
+  return <div className={props.className} onClick={props.onClick} />;
+}
+
+function PrevArrow(props) {
+  return <div className={props.className} onClick={props.onClick} />;
+}
+
+// settings for the react-slick
+const settings = {
+  dots: true,
+  infinite: false,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 500,
+  centerMode: true,
+  focusOnSelect: true,
+  centerPadding: "20%",
+  appendDots: (dots) => (
+    <div>
+      <ul className="dot-container"> {dots} </ul>
+    </div>
+  ),
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+};
+
+// the function that will return this component
 function ExpertiseArea() {
-	const [categories, setCategories] = React.useState([]);
-	const [services, setServices] = React.useState([]);
-  const [selected, setSelected] = React.useState('');
+  const [categories, setCategories] = React.useState([]);
+  const [services, setServices] = React.useState([]);
+  const [selected, setSelected] = React.useState("");
 
   const loadDb = async () => {
-		// get the data from the db
-		const res = await axios.get("http://localhost:3001/api/areas");
-		db = res.data;
-		// if the db has at least one item...
-    if(db){
-			// set the selected category as the first one
-			setSelected(res.data[0].Category);
+    // get the data from the db
+    const res = await axios.get("http://localhost:3001/api/areas");
+    db = res.data;
+    // if the db has at least one item...
+    if (db) {
+      // set the selected category as the first one
+      setSelected(res.data[0].Category);
 
-			// create an empty array and set it as an array with all the categories names without repeating them
-			const categoriesAux = [];
-			db.forEach((item)=>{
-				if(!categoriesAux.includes(item.Category)){
-					categoriesAux.push(item.Category);
-				}
-			});
-			setCategories(categoriesAux);
+      // create an empty array and set it as an array with all the categories names without repeating them
+      const categoriesAux = [];
+      db.forEach((item) => {
+        if (!categoriesAux.includes(item.Category)) {
+          categoriesAux.push(item.Category);
+        }
+      });
+      setCategories(categoriesAux);
     }
   };
 
-	//call loadDb when the page is loaded
+  //call loadDb when the page is loaded
   React.useEffect(() => {
     loadDb();
-	}, []);
-	
-	const handleButtonClick = (e)=>{
-		//set selected as the text within the button clicked
-    setSelected(e.target.innerText);
-  }
+  }, []);
 
-	// when selected changes...
+  const handleButtonClick = (e) => {
+    //set selected as the text within the button clicked
+    setSelected(e.target.innerText);
+  };
+
+  // when selected changes...
   React.useEffect(() => {
-		//create an aux array and set it as an array with all the items from the t=db that have the selected category
-    const aux = db.filter((item)=>{
-			return item.Category === selected;
-		});
+    //create an aux array and set it as an array with all the items from the db that have the selected category
+    const aux = db.filter((item) => {
+      return item.Category === selected;
+    });
     setServices(aux);
   }, [selected]);
 
@@ -58,23 +88,38 @@ function ExpertiseArea() {
     <section className="ExpertiseArea">
       <h2>Áreas de atuação</h2>
       <div className="categories">
-        {categories.map((item, index)=>{
-          return <button key={index} onClick={handleButtonClick}>{item}</button>;
+        {categories.map((item, index) => {
+          return item === selected ? (
+            <button
+              key={index}
+              onClick={handleButtonClick}
+              className="selected"
+            >
+              {item}
+            </button>
+          ) : (
+            <button key={index} onClick={handleButtonClick}>
+              {item}
+            </button>
+          );
         })}
       </div>
       <div className="services">
-			<Slider dots={true} infinite={true} arrows={true} slidesToShow={1} slidesToScroll={1} centerMode={true} focusOnSelect={true}>
-				{services.map((item, index)=>{
-					return(
-						<Service key={index} img={item.image.url} title={item.Title} text={item.Text}/>
-					);
-				})}
-			</Slider>
+        <Slider {...settings} className="Slider">
+          {services.map((item, index) => {
+            return (
+              <Service
+                key={index}
+                img={item.image.url}
+                title={item.Title}
+                text={item.Text}
+              />
+            );
+          })}
+        </Slider>
       </div>
-      <div className="dots"></div>
     </section>
   );
 }
 
 export default ExpertiseArea;
-
--- client/src/components/Service/Service.jsx
+++ client/src/components/Service/Service.jsx
@@ -5,8 +5,10 @@ import "./Service.css";
 function Service(props) {
   return (
     <div className="Service">
-      <img src={props.img} alt={"Imagem que representa " + props.title} />
-      <h4>{props.title}</h4>
+      <div className="img-container">
+        <img src={props.img} alt={"Imagem que representa " + props.title} />
+      </div>
+      <h3>{props.title}</h3>
       <p>{props.text}</p>
     </div>
   );
