1d4fcff141711e9ad592027ce04ca541c6ff36e7
Vinícius Scala <viniciusscala@gmail.com>
2020-07-12 06:23:33 -0300

    Feature(feature/call-to-blog) : Add html and css

--- client/src/components/CallToBlog/CallToBlog.jsx
+++ client/src/components/CallToBlog/CallToBlog.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 import "./CallToBlog.css";
 
-function CallToBlog(){
-    return(
-
-    );
+function CallToBlog() {
+  return (
+    <section className="CallToBlog">
+      <div className="CallToBlog-container">
+        <h2>Visite agora nosso blog!</h2>
+        <p>
+          Aprenda mais sobre lorem ipsum dolor sit amet, consectetur adipiscing
+        </p>
+        <button>Visitar blog</button>
+      </div>
+    </section>
+  );
 }
 
 export default CallToBlog;
