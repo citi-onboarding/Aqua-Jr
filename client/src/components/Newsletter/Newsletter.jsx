d135a9818bb1336609abf9829d99fe28a37b24ee
Breno Cavalcanti <breno.uc.cavalcanti@gmail.com>
2020-07-12 22:04:33 -0300

    (feature/newsletter-front): added the first version of the backgound

--- client/src/components/Newsletter/Newsletter.jsx
+++ client/src/components/Newsletter/Newsletter.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
+import './Newsletter.css';
+
 function Newsletter() {
 
     return (
+    <div className= "Body-Newsletter">
 
+        <div className= "Text-Newsletter">
+            <h1>
+                Inscreva-se na nossa Newsletter!
+            </h1>
+            <p>
+            Por ela você irá receber conteúdo exclusivo de possibilidade de receber descontos em nossos serviços!
+            </p>
+        </div>
+
+
+        <form className= "Form-Newsletter">
+
+            <label>Email</label>
+            <input type="Email"/>
+            <button>Submit</button>
+        </form>
+    </div>
     )
 }
 
