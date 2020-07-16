8c59331536c645fddaf7cf1a0b289ef17d98516c
Vinícius Scala <viniciusscala@gmail.com>
2020-07-16 17:28:22 -0300

    feature(navbar) : Finish everything

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,50 @@
 import React from "react";
-import logo from "./logo.png"
+import logo from "./logo.png";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
-function Navbar(){
-    return(
-        <nav className="Navbar">
-            <img src={logo} alt="Aqua jr logo"/>
-            <div className="links">
-                <a href="#">Início</a>
-                <a href="#">Áreas de atuação</a>
-                <a href="#">Sobre nós</a>
-                <a href="#">Depoimentos</a>
-                <a href="#">Contato</a>
-            </div>
-            <button className="blog-button">Blog</button>
-            <button className="sandwich">
-                <div></div>
-                <div></div>
-                <div></div>
-            </button>
-        </nav>
-    );
+function handleClick() {
+  document.querySelector(".Navbar .side-menu").classList.toggle("active");
+  document.querySelector(".sandwich").classList.toggle("active");
+}
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <img src={logo} alt="Aqua jr logo" />
+
+      <div className="web-only">
+        <div className="links">
+          <a href="#">Início</a>
+          <a href="#">Áreas de atuação</a>
+          <a href="#">Sobre nós</a>
+          <a href="#">Depoimentos</a>
+          <a href="#">Contato</a>
+        </div>
+        <a className="blog-button" href="https://aquajr.wixsite.com/aquajr/blog">Blog</a>
+      </div>
+
+      <button className="sandwich" onClick={handleClick}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      <aside className="side-menu">
+        <div className="side-menu-container">
+          <div className="links">
+            <a href="#">Início</a>
+            <a href="#">Áreas de atuação</a>
+            <a href="#">Sobre nós</a>
+            <a href="#">Depoimentos</a>
+            <a href="#">Contato</a>
+          </div>
+          <a className="blog-button" href="https://aquajr.wixsite.com/aquajr/blog">Blog</a>
+        </div>
+      </aside>
+			
+    </nav>
+  );
 }
 
 export default Navbar;
