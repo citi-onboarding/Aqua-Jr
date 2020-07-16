06b3f4b67011962596b65ed59e8c2a71ca2c99b2
Vinícius Scala <viniciusscala@gmail.com>
2020-07-16 01:33:31 -0300

    feature(navbar) : Add html into the .jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Navbar(){
-
+    return(
+        <nav className="Navbar">
+            <img src="./logo.png" alt="Aqua jr logo"/>
+            <div className="links">
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+            <button>Blog</button>
+            <div className="sandwich"></div>
+        </nav>
+    );
 }
 
 export default Navbar;
