7291089b3d7ee87ffa836cb31f4a25f36640623a
sofiadinizms <sdms@cin.ufpe.br>
2020-07-29 19:31:47 -0300

    added blue waves

--- client/src/components/CallToBlog/CallToBlog.jsx
+++ client/src/components/CallToBlog/CallToBlog.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./CallToBlog.css";
-import onda1 from "./onda1 (3).svg";
+import ondasAzuis from './ilustracao (2).svg';
+import onda2 from './onda2 (4).svg';
 
 function CallToBlog() {
   return (
     <section className="CallToBlog">
+      <div className= "OndasAzuis">
+        <img src={ondasAzuis}/>
+      </div>
       <div className="CallToBlog-container">
         <h2>Visite agora nosso blog!</h2>
         <p>
@@ -12,8 +16,12 @@ function CallToBlog() {
         </p>
         <button name= "Visitar o blog">Visitar blog</button>
       </div>
+      <div className="OndaBottom">
+        <img src={onda2}/>
+      </div>
     </section>
   );
 }
 
 export default CallToBlog;
+
