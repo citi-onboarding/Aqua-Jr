24b3369acea263ca01855cb026a2d165ff7e3780
Breno Cavalcanti <breno.uc.cavalcanti@gmail.com>
2020-07-31 15:05:58 -0300

    (hotfix-all-2): remove userffect not used

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState}  from 'react';
 import './Contact.css';
 import img from "./contact-img.png";
 import axios from 'axios';
